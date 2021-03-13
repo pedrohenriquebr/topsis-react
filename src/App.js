39521d90f20d286bcf35c4e16d81f21ceb396edd
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2021-03-13 20:37:41 -0300

    Change Layout to Stepper

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from '@material-ui/core/Container';
 import Typography  from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import CriteriaTable from './CriteriaTable';
-import Form from './Form';
-import Grid from './Grid';
-import GridForm from './GridForm';
-import topsis_predict from "./topsis";
+import LinearStepper from './LinearStepper';
 
-const useStyles = makeStyles(theme => ({
-  root : {
-    margin: theme.spacing(6,0,3)
-  },
-}));
-
-const criteriaToDataColumns = (criteria) => criteria.map((d) => (
-  {
-    field: d.criterionName,
-    headerName: [...d.criterionName[0].toUpperCase(), d.criterionName.slice(1)],
-    type: 'number'
-  }
-));
 function App() {
   const criteria = [
     { name: "Price", type: 1, weight: 0.1 },
@@ -38,62 +19,14 @@ function App() {
     [1368, 7.7, 8.6, 8.3, 4.6],
   ];
 
-  const [state, setState] = useState({
-    criteria: [],
-    sum: 0,
-    disabledForm: false,
-    disableGrid: true,
-    dataset: []
-  });
-
-  const  styles  = useStyles();
-  
-  const removeRow = (idx) => {
-    const nextSum = state.sum - parseInt(state.criteria[idx].weight);
-    setState({...state,
-      criteria: [...state.criteria.filter((d,i) => i !== idx)],
-      sum: nextSum,
-      disabledForm: nextSum >= 100,
-      disableGrid: nextSum === 0
-    });
-  };
-
-  const handleSubmit = (row) => {
-    //check the weights
-    const nextSum = state.sum + parseInt(row.weight);
-    setState({...state,
-      criteria: [...state.criteria, {...row, weight: parseInt(row.weight)}],
-      sum: nextSum,
-      disabledForm: nextSum >= 100,
-      disableGrid: false
-    });
-    
-  };
-
-  const handleGridSubmit = (row) => {
-    setState({...state,
-      dataset: [...state.dataset, row]
-    })
-  }
-
   return (
     <Container maxWidth="md">
        <Box my={4} >
         <Typography variant="h4" component="h1">
               TOPSIS React
           </Typography>
-        <Box my={4} display="flex" flexDirection="column" width={500}>
-          <CriteriaTable  rows={state.criteria} removeRow={removeRow}/>
-          <Form disabled={state.disabledForm} handleSubmit={handleSubmit} />
-        </Box>
-        {
-        !state.disableGrid && 
-          <Box >
-          <Grid dataset={state.dataset} columns={criteriaToDataColumns(state.criteria)} />
-          <GridForm disabled={state.disableGrid} handleSubmit={handleGridSubmit} />
-          </Box>
-        }
-       </Box>
+      <LinearStepper />
+      </Box>
     </Container>
   );
 }
---
+++ src/LinearStepper.js
@@ -0,0 +1,165 @@
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import CriteriaTable from "./CriteriaTable";
+import Form from "./Form";
+import Grid from "./Grid";
+import GridForm from "./GridForm";
+import topsis_predict from "./topsis";
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(6, 0, 3),
+    width: "100%",
+  },
+  button: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+function getSteps() {
+  return ["Insira os critérios", "Insira as alternativas", "Veja o resulado"];
+}
+
+const criteriaToDataColumns = (criteria) =>
+  criteria.map((d) => ({
+    field: d.criterionName,
+    headerName: [...d.criterionName[0].toUpperCase(), d.criterionName.slice(1)],
+    type: "number",
+  }));
+
+export default function LinearStepper(props) {
+  const classes = useStyles();
+  const [state, setState] = useState({
+    criteria: [],
+    sum: 0,
+    disabledForm: false,
+    disableGrid: true,
+    dataset: [],
+  });
+
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = getSteps();
+
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return (
+          <Box>
+            <CriteriaTable rows={state.criteria} removeRow={removeRow} />
+            <Form disabled={state.disabledForm} handleSubmit={handleSubmit} />
+          </Box>
+        );
+      case 1:
+        return (
+          <Box>
+            <Grid
+              dataset={state.dataset}
+              columns={criteriaToDataColumns(state.criteria)}
+            />
+            <GridForm
+              disabled={state.disableGrid}
+              handleSubmit={handleGridSubmit}
+            />
+          </Box>
+        );
+      default:
+        return <Typography>Etapa desconhecida</Typography>;
+    }
+  }
+  
+  const handleNext = () => {
+    setActiveStep((prev) => prev + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prev) => prev - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const removeRow = (idx) => {
+    const nextSum = state.sum - parseInt(state.criteria[idx].weight);
+    setState({
+      ...state,
+      criteria: [...state.criteria.filter((d, i) => i !== idx)],
+      sum: nextSum,
+      disabledForm: nextSum >= 100,
+      disableGrid: nextSum === 0,
+    });
+  };
+
+  const handleSubmit = (row) => {
+    //check the weights
+    const nextSum = state.sum + parseInt(row.weight);
+    setState({
+      ...state,
+      criteria: [...state.criteria, { ...row, weight: parseInt(row.weight) }],
+      sum: nextSum,
+      disabledForm: nextSum >= 100,
+      disableGrid: false,
+    });
+  };
+
+  const handleGridSubmit = (row) => {
+    setState({ ...state, dataset: [...state.dataset, row] });
+  };
+
+  return (
+    <Box className={classes.root}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, idx) => {
+          const stepProps = {};
+          const labelProps = {};
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      {activeStep === steps.length ? (
+        <div>
+          <Typography className={classes.instructions}>
+            Todas as etapas completadas
+          </Typography>
+          <Button onClick={handleReset} className={classes.button}>
+            Reset
+          </Button>
+        </div>
+      ) : (
+        <div>
+          <div>{getStepContent(activeStep)}</div>
+          <div>
+            <Button
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              className={classes.button}
+            >
+              Back
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleNext}
+              className={classes.button}
+            >
+              {activeStep === steps.length - 1 ? "Finalizar" : "Próximo"}
+            </Button>
+          </div>
+        </div>
+      )}
+    </Box>
+  );
+}
