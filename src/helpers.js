8e94adbcaa0e889a42a53bf83f3b5b8819811152
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2021-03-14 15:22:26 -0300

    Add helpers

---
+++ src/helpers.js
@@ -0,0 +1,9 @@
+export function criteriaToDataColumns (criteria){
+    return [{field: 'Nome', headerName: 'Nome', width: 200},
+    ...criteria.map((d) => ({
+    field: d.criterionName,
+    headerName: [...d.criterionName[0].toUpperCase(), d.criterionName.slice(1)],
+    type: "number",
+    width: 160 
+  }))]
+}
