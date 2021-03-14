e57850e9c7904bceec0c9a93bcecbed1f3c8a266
Pedro Henrique Braga da Silva <pedrohenriquebraga735@gmail.com>
2021-03-14 15:22:45 -0300

    Format identation for Grid component

--- src/Grid.js
+++ src/Grid.js
@@ -2,6 +2,10 @@ import { DataGrid } from '@material-ui/data-grid';
 export default function Grid(props){
     return (
       <div style={{width: '100%', height:300}}>
-          <DataGrid rows={props.dataset} columns={props.columns} pageSize={5} />
+          <DataGrid 
+          rows={props.dataset} 
+          columns={props.columns} 
+          pageSize={5} 
+          loading={props.loading}/>
       </div>);
 }
